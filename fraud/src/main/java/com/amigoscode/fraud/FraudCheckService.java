78bb4aaeb31d0f2a41d1017c53d0f96db9243cba
Roman Markov <enjoi328@gmail.com>
2023-03-14 11:30:52 +0400

    commit at end 2nd video

---
+++ fraud/src/main/java/com/amigoscode/fraud/FraudCheckService.java
@@ -0,0 +1,23 @@
+package com.amigoscode.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFraudulentCustomer(Integer customerId) {
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .createdAt(LocalDateTime.now())
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .build()
+        );
+        return false;
+    }
+}
